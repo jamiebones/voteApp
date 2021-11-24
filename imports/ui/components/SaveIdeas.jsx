ac79d7ec8eccb7da428f80d21f61971a3fbd45a6
James Oshomah <Jamiebones2000@yahoo.co.uk>
2021-11-24 12:38:52 +0100

    created the ui for inserting data

---
+++ imports/ui/components/SaveIdeas.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { IdeasCollection } from "../../api/ideas/ideas";
+
+export default SaveIdeas = () => {
+  const [idea, setIdea] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (idea.length > 10) {
+      IdeasCollection.insert({ idea : idea }, (err, ideaId) => {
+        if (err) {
+          alert(err);
+        } else {
+          alert(ideaId);
+        }
+      });
+      setIdea("");
+    } else {
+      alert("Idea must be at least 10 characters long");
+    }
+  };
+
+  return (
+    <div>
+      <textarea
+        id="idea"
+        name="idea"
+        value={idea}
+        cols="60"
+        rows="10"
+        onChange={(event) => setIdea(event.target.value)}
+        placeholder="Enter your idea here..."
+      ></textarea>
+      <br />
+      <button
+        onClick={handleSubmit}
+        id="saveIdea"
+        style={{ padding: 15 + "px", fontSize: 20 + "px" }}
+        className="btn btn-primary"
+      >
+        Save Idea
+      </button>
+    </div>
+  );
+};
--- imports/ui/pages/App.jsx
+++ imports/ui/pages/App.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import SaveIdea from './components/SaveIdea';
+import React from "react";
+import SaveIdea from "../components/SaveIdeas";
 
 export const App = () => {
   return (
     <div>
       <h1>Vote Application</h1>
-
       <SaveIdea />
-
     </div>
-
-
   );
-
-}
- 
+};
